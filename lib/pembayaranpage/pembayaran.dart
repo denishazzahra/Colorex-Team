2270b64b511d4befc13fa974c402f33cbc67bab8
Kavarera <rafli.030715@gmail.com>
2024-06-27 17:46:24 +0700

    fixing pembayaran page

--- lib/pembayaranpage/pembayaran.dart
+++ lib/pembayaranpage/pembayaran.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 import '../questionpage/questionpage.dart';
+import '../widget/costum_button.dart';
 
 class PembayaranPage extends StatelessWidget {
   const PembayaranPage({super.key});
@@ -9,21 +11,29 @@ class PembayaranPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: Column(
+          child: Stack(
         children: [
           Image.asset('assets/img/qrcode.jpg'),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const QuestionPage()));
-                },
-                child: const Text('Konfirmasi Pembayaran'),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SizedBox(
+                width: double.infinity,
+                child: Center(
+                  child: MyCostumButtton1(
+                    buttonColor: Theme.of(context).primaryColor,
+                    buttonText: "Confirm",
+                    func: () {
+                      Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const QuestionPage()));
+                    },
+                  ),
+                ),
               ),
             ),
           ),
